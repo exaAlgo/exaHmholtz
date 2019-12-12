2dfc4625b0adb4cd14add18273032704a0dedd42
Thilina Rathnayake <thilinarmtb@gmail.com>
2019-12-11 20:37:46 -0600

    [cg] polish CG test

--- tests/t20-exa-hmholtz-pcg.c
+++ tests/t20-exa-hmholtz-pcg.c
@@ -15,7 +15,8 @@ int populate(exaScalar *q,exaUInt size);
 
 int main(int argc,char *argv[])
 {
-  exaUInt size=2;
+  srand(time(NULL)); // initialize the random number generator
+  exaUInt size=10; // set problem size
 
   MPI_Init(NULL,NULL);
 
@@ -24,15 +25,11 @@ int main(int argc,char *argv[])
     exit(0);
   }
 
-  // Create exaHandle
   exaHandle h; exaInit(&h,MPI_COMM_WORLD,argv[1]);
-  // Initialize settings
   exaSettings s; exaSettingsInit(h,NULL,&s);
-  // Create hmholtz solver
   exaHmholtz hmhz; exaHmholtzCreate(h,s,&hmhz);
-  // Create program
+
   exaProgram p; exaProgramCreate(h,argv[0],s,&p);
-  // Create kernel
   exaKernelCreate(p,"Ax",&kernelAx);
 
   // set weights to all ones
@@ -42,15 +39,7 @@ int main(int argc,char *argv[])
   for(i=0;i<size;i++) in[i]=1.0;
   exaVectorWrite(one,in);
 
-  srand(time(NULL));
   exaScalar *xx; exaCalloc(size,&xx); populate(xx,size);
-#if 0
-  printf("xx=\n");
-  for(int i=0;i<size;i++){
-    printf("%lf ",xx[i]);
-  }
-  printf("\n");
-#endif
 
   // create matrix A - make sure it is SPD
   exaScalar *A; exaCalloc(size*size,&A); populate(A,size*size);
@@ -63,44 +52,26 @@ int main(int argc,char *argv[])
     }
     A[i*size+i]+=sum;
   }
-
-#if 0
-  printf("A=\n");
-  for(int i=0;i<size;i++){
-    for(int j=0;j<size;j++){
-      //A[i*size+j]=(i>j);
-      printf("%lf ",A[i*size+j]);
-    }
-    printf("\n");
-  }
-#endif
   exaVectorCreate(h,size*size,&Amatrix); exaVectorWrite(Amatrix,A);
 
+  // set RHS
   exaScalar *Axx; exaCalloc(size,&Axx); matVec(A,xx,size,Axx);
-#if 0
-  printf("Axx=\n");
-  for(int i=0;i<size;i++){
-    printf("%lf ",Axx[i]);
-  }
-  printf("\n");
-#endif
-
   exaVector b; exaVectorCreate(h,size,&b); exaVectorWrite(b,Axx);
 
-  // Call pcg
+  // Call CG with zero initial guess
   exaVector x; exaVectorCreate(h,size,&x);
   for(i=0;i<size;i++) in[i]=0.0;
   exaVectorWrite(x,in);
+  int nIter=exaHmholtzCG(x,calcAx,b,one,1e-9,100,hmhz);
 
-  int nIter=exaHmholtzCG(x,calcAx,b,one,1e-7,100,hmhz);
+  // nIter <= size should hold true
+  assert(nIter<=size);
 
-  // get answer back
+  // compare answer
   exaScalar *out; exaCalloc(size,&out); exaVectorRead(x,out);
-
-  // compare answer;
   for(i=0;i<size;i++)
-    if(fabs(out[i]-xx[i])>1e-6)
-      fprintf(stderr,"%lf != %lf\n",out[i],xx[i]);
+    if(fabs(out[i]-xx[i])>1e-8)
+      fprintf(stderr,"Error: %lf != %lf\n",out[i],xx[i]);
 
   exaFree(xx); exaFree(A); exaFree(Axx); exaFree(in); exaFree(out);
   exaDestroy(Amatrix); exaDestroy(x); exaDestroy(b); exaDestroy(one);
