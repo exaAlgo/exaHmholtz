5136239db3c84df99051c678406087f5d7d39721
Thilina Rathnayake <thilinarmtb@gmail.com>
2020-06-18 04:32:21 -0400

    Fix an issue with gs kernels

--- src/exa-hmholtz-gather-scatter.c
+++ src/exa-hmholtz-gather-scatter.c
@@ -4,11 +4,23 @@
 //
 int exaHmholtzGatherScatter(exaVector in,exaMesh mesh,exaHmholtz hmhz){
   exaGS dGs=mesh->dGs;
-
   exaVector indices; exaGSGetIndices(dGs,&indices);
   exaVector offsets; exaGSGetOffsets(dGs,&offsets);
-  exaUInt size=exaVectorGetSize(offsets)-1;
 
-  exaKernelRun(hmhz->gatherScatter,getExaUInt(size),offsets,indices,in);
+  exaHandle h=hmhz->h; exaUInt size=exaVectorGetSize(offsets)-1;
+  exaVector q; exaVectorCreate(h,size,exaScalar_t,&q);
+
+  exaKernelRun(hmhz->gather,getExaUInt(size),offsets,indices,in,q);
+
+  exaScalar *qh; exaVectorRead(q,(void**)&qh);
+  exaBuffer buf; exaBufferCreate(&buf,1024);
+  exaGSOp((void*)qh,exaScalar_t,exaAddOp,0,dGs,buf);
+  exaVectorWrite(q,(void*)qh);
+
+  exaKernelRun(hmhz->scatter,getExaUInt(size),offsets,indices,in,q);
+
+  exaDestroy(q  );
+  exaDestroy(buf);
+
   return 0;
 }
